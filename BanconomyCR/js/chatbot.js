95382f6c7f2c9d95d3b2a7acf4320a3c59a1f1fb
gloryaj <gloryaj99@gmail.com>
2024-06-26 12:01:05 -0600

    chat responsive

--- BanconomyCR/js/chatbot.js
+++ BanconomyCR/js/chatbot.js
@@ -1,88 +1,72 @@
-//Instrucciones
-// Para implementar este script inserte este script en el body de la página
-// El token de la URL se encuentra en el canal Mobile de Virtual Agents.
-
-
-// Specifies the token endpoint URL. 
+// Especifica la URL del token endpoint.
 const tokenEndpointURL = new URL('https://defaulte7984cac25434f888f979524335e6b.c4.environment.api.powerplatform.com/powervirtualagents/botsbyschema/cr29b_banconomyBot/directline/token?api-version=2022-03-01-preview');
 
+// Crear el elemento de botón
+var boton = document.createElement("button");
+boton.id = "botonToggle";
+boton.textContent = "Chat";
+boton.onclick = toggleVentana;
 
-//Inserta html del chatbot
+// Crear el elemento de ventana flotante
+var popUp = document.createElement("div");
+popUp.id = "popUp";
 
-    // Crear el elemento de botón
-  var boton = document.createElement("button");
-  boton.id = "botonToggle";
-  boton.textContent = "Chat";
-  boton.onclick = toggleVentana;
+// Crear el elemento de banner
+var banner = document.createElement("div");
+banner.id = "banner";
+var bannerTexto = document.createElement("p");
+bannerTexto.textContent = "Banconomy Bot";
+banner.appendChild(bannerTexto);
 
-  // Crear el elemento de ventana flotante
-  var popUp = document.createElement("div");
-  popUp.id = "popUp";
+// Crear el elemento de webchat
+var webchat = document.createElement("div");
+webchat.id = "webchat";
+webchat.setAttribute("role", "main");
 
-  // Crear el elemento de banner
-  var banner = document.createElement("div");
-  banner.id = "banner";
-  var bannerTexto = document.createElement("p");
-  bannerTexto.textContent = "Banconomy Bot";
-  banner.appendChild(bannerTexto);
+// Agregar los elementos al popUp
+popUp.appendChild(banner);
+popUp.appendChild(webchat);
 
-  // Crear el elemento de webchat
-  var webchat = document.createElement("div");
-  webchat.id = "webchat";
-  webchat.setAttribute("role", "main");
+// Agregar el botón y la ventana al documento
+document.body.appendChild(boton);
+document.body.appendChild(popUp);
 
-  // Agregar los elementos al popUp
-  popUp.appendChild(banner);
-  popUp.appendChild(webchat);
+// Agrega funciones pop up
+var init_win_toggle = document.getElementById("popUp");
+init_win_toggle.style.display="none";
+function toggleVentana() {
+  var ventana = document.getElementById("popUp");
+  var botonToggle = document.getElementById("botonToggle");
 
-  // Agregar el botón y la ventana al documento
-  document.body.appendChild(boton);
-  document.body.appendChild(popUp);
+  if (ventana.style.display === "none" || ventana.style.display === "") {
+    ventana.style.display = "block";
+    botonToggle.innerText = "Ocultar";
+  } else {
+    ventana.style.display = "none";
+    botonToggle.innerText = "Chat";
+  }
+}
 
+// Función para cerrar la ventana flotante
+function cerrarVentana() {
+  var ventana = document.getElementById("popUp");
+  var botonToggle = document.getElementById("botonToggle");
 
-//Agrega funciones pop up
-  var init_win_toggle = document.getElementById("popUp");
-        init_win_toggle.style.display="none"
-       function toggleVentana() {
-            var ventana = document.getElementById("popUp");
-            var botonToggle = document.getElementById("botonToggle");
-
-            if (ventana.style.display === "none" || ventana.style.display === "") {
-                ventana.style.display = "block";
-                botonToggle.innerText = "Ocultar";
-            } else {
-                ventana.style.display = "none";
-                botonToggle.innerText = "Chat";
-            }
-        }
-
-        // Función para cerrar la ventana flotante
-        function cerrarVentana() {
-            var ventana = document.getElementById("popUp");
-            var botonToggle = document.getElementById("botonToggle");
-
-            ventana.style.display = "none";
-            botonToggle.innerText = "Chat";
-        }
-
+  ventana.style.display = "none";
+  botonToggle.innerText = "Chat";
+}
 
 // Crear el script y configurar sus atributos
+var scriptElement = document.createElement("script");
+scriptElement.crossorigin = "anonymous";
+scriptElement.src = "https://cdn.botframework.com/botframework-webchat/latest/webchat.js";
 
-  var scriptElement = document.createElement("script");
-  scriptElement.crossorigin = "anonymous";
-  scriptElement.src = "https://cdn.botframework.com/botframework-webchat/latest/webchat.js";
+// Agregar el script al final del cuerpo del documento
+document.body.appendChild(scriptElement);
 
-  // Agregar el script al final del cuerpo del documento
-  document.body.appendChild(scriptElement);
-
-
-//Configuracion chatbot virtual agents
-
+// Configuración del chatbot de Virtual Agents
 (async function () {
-  // Specifies style options to customize the Web Chat canvas.
-  // Please visit https://microsoft.github.io/BotFramework-WebChat for customization samples.
   const styleOptions = {
-    // Hide upload button.
     hideUploadButton: true
   };
 
@@ -95,7 +79,6 @@ const tokenEndpointURL = new URL('https://defaulte7984cac25434f888f979524335e6b.
         if (!response.ok) {
           throw new Error('Failed to retrieve regional channel settings.');
         }
-
         return response.json();
       })
       .then(({ channelUrlsById: { directline } }) => directline),
@@ -104,7 +87,6 @@ const tokenEndpointURL = new URL('https://defaulte7984cac25434f888f979524335e6b.
         if (!response.ok) {
           throw new Error('Failed to retrieve Direct Line token.');
         }
-
         return response.json();
       })
       .then(({ token }) => token)
@@ -132,88 +114,110 @@ const tokenEndpointURL = new URL('https://defaulte7984cac25434f888f979524335e6b.
   WebChat.renderWebChat({ directLine, locale, styleOptions }, document.getElementById('webchat'));
 })();
 
+// Estilos
+var estilo = document.createElement("style");
+estilo.type = "text/css";
 
-//Estilos
-  // Crear el elemento de estilo
-  var estilo = document.createElement("style");
-  estilo.type = "text/css";
+var css = `
+  #popUp {
+    position: fixed;
+    bottom: 70px;
+    right: 10px;
+    height: 520px;
+    max-width: 1100px;
+    z-index: 999;
+    background-color: white;
+    border-radius: 20px;
+    box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
+  }
 
-  // Definir el contenido del estilo con el CSS proporcionado
-  var css = `
+  #botonToggle {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    z-index: 1000;
+    background-color: #ffffff;
+    color: #333;
+    padding: 10px 20px;
+    border: 2px solid #333;
+    border-radius: 20px;
+    cursor: pointer;
+    font-size: 14px;
+    outline: none;
+    transition: background-color 0.3s, color 0.3s;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  }
+
+  #botonToggle:hover {
+    background-color: #FD7E14;
+    color: #ffffff;
+    box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
+  }
+
+  #banner {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    background-color: #FD7E14;
+    display: flex;
+    height: 50px;
+    border-radius: 20px 20px 0 0;
+    box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
+    color: white;
+  }
+
+  #banner p {
+    display: inline-block;
+    vertical-align: middle;
+    margin: auto;
+  }
+
+  #webchat {
+    top: 50px;
+    width: 100%;
+    height: 450px;
+    padding: 10px;
+    border-radius: 20px;
+  }
+
+  #webchat p {
+    font-size: 17px;
+  }
+
+  .webchat__text-content {
+    background-color: whitesmoke;
+    border-radius: 5px;
+  }
+
+  @media (max-width: 768px) {
     #popUp {
-      position: fixed;
-      bottom: 70px;
-      right: 10px;
-      height: 520px;
-      max-width: 1100px;
-      z-index: 999;
-      background-color: white;
-      border-radius: 20px;
-      box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
-    }
-
-    #botonToggle {
-      position: fixed;
       bottom: 20px;
-      right: 20px;
-      z-index: 1000;
-      background-color: #ffffff;
-      color: #333;
-      padding: 10px 20px;
-      border: 2px solid #333;
-      border-radius: 20px;
-      cursor: pointer;
-      font-size: 14px;
-      outline: none;
-      transition: background-color 0.3s, color 0.3s;
-      box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-    }
-
-    #botonToggle:hover {
-      background-color: #FD7E14;
-      color: #ffffff;
-      box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
-    }
-
-    #banner {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      text-align: center;
-      background-color: #FD7E14;
-      display: flex;
-      height: 50px;
-      border-radius: 20px 20px 0 0;
-      box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
-      color: white;
-    }
-
-    #banner p {
-      display: inline-block;
-      vertical-align: middle;
-      margin: auto;
+      right: 10px;
+      width: 90%;
+      height: 400px;
     }
 
     #webchat {
-      top: 50px;
-      width: 1000px;
-      height: 450px;
-      padding: 10px;
-      border-radius: 20px;
+      height: 350px;
+    }
+  }
+
+  @media (max-width: 480px) {
+    #popUp {
+      bottom: 20px;
+      right: 5px;
+      width: 95%;
+      height: 300px;
     }
 
-    #webchat p {
-      font-size: 17px;
+    #webchat {
+      height: 250px;
     }
+  }
+`;
 
-    .webchat__text-content {
-      background-color: whitesmoke;
-      border-radius: 5px;
-    }
-  `;
+estilo.appendChild(document.createTextNode(css));
 
-  // Asignar el contenido del CSS al elemento de estilo
-  estilo.appendChild(document.createTextNode(css));
-
-  // Agregar el elemento de estilo al head del documento
-  document.head.appendChild(estilo);
+// Agregar el elemento de estilo al head del documento
+document.head.appendChild(estilo);
